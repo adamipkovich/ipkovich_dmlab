1fb126262c9a860fda00f610428c7c8b2da88e3f
Adam Ipkovich <71510025+adamipkovich@users.noreply.github.com>
2024-11-29 15:23:24 +0100

    functionalized

--- data_request.py
+++ data_request.py
@@ -4,29 +4,37 @@ import zipfile
 import pandas as pd
 import kaggle
 
-
+def pull_kaggle_data(name : str) -> None:
+    """Downloads kaggle dataset (uses kaggle.api)
+    :args
+        name (str) : name of the kaggle dataset  {user}/{dataset_name}
+    """
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files("pavansubhasht/ibm-hr-analytics-attrition-dataset", path = "temp")
+    
+def unzip_kaggle_data(loc : str):
+    """This function finds all .zip files in the temp folder"""
+    
+    for dir in listdir(loc): ## This is a placeholder function if more than one dataset should be used.
+        if dir.endswith(".zip"): ## find all zips in temp
+            with zipfile.ZipFile(os.path.join(loc, dir), "r") as zip_file:
+                zip_file.extractall("temp")
+def read_kaggle_data(loc : str) -> dict:
+    data = dict()
+    for dir in listdir(loc): # I do this with consideration to multiple csv files in one zip
+        if dir.endswith(".csv"): ## All .csv to memory
+            data[dir.removesuffix(".csv")] = pd.read_csv(os.path.join(loc, dir))
+            
+    return data
 ## Test
 if __name__ == "__main__":
     ## create a decorator
     ## create temp...
-    kaggle.api.authenticate()
-    kaggle.api.dataset_download_files("pavansubhasht/ibm-hr-analytics-attrition-dataset", path = "temp")
-    ## This code finds all zips and decompresses them to the local temp folder.
-    ## find all zips in temp
-    data = dict()
-    
     loc = os.path.join(getcwd(), "temp") 
-    for dir in listdir(loc): ## This is a placeholder function if more than one dataset should be used.
-        if dir.endswith(".zip"):
-            with zipfile.ZipFile(os.path.join(loc, dir), "r") as zip_file:
-                zip_file.extractall("temp")
-                
-                
-    ## All .csv to memory
-    for dir in listdir(loc): # I do this with consideration to multiple csv files in one zip
-        if dir.endswith(".csv"):
-            data[dir.removesuffix(".csv")] = pd.read_csv(os.path.join(loc, dir))
-            
+    pull_kaggle_data("pavansubhasht/ibm-hr-analytics-attrition-dataset")
+    unzip_kaggle_data(loc) 
+    data = read_kaggle_data(loc)
+  
     ##send to postgres
     
     ##close connection, and delete temp
