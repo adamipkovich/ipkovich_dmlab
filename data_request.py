40d50c3630749863563676ecda45871022adbb7f
Adam Ipkovich <71510025+adamipkovich@users.noreply.github.com>
2024-11-29 15:15:56 +0100

    Read into memory

--- data_request.py
+++ data_request.py
@@ -1,9 +1,8 @@
 import os
 from os import listdir,getcwd
 import zipfile
-
+import pandas as pd
 import kaggle
-
 
 
 ## Test
@@ -14,11 +13,23 @@ if __name__ == "__main__":
     kaggle.api.dataset_download_files("pavansubhasht/ibm-hr-analytics-attrition-dataset", path = "temp")
     ## This code finds all zips and decompresses them to the local temp folder.
     ## find all zips in temp
-    loc = os.path.join(getcwd(), "temp")
-    for dir in listdir(loc):
+    data = dict()
+    
+    loc = os.path.join(getcwd(), "temp") 
+    for dir in listdir(loc): ## This is a placeholder function if more than one dataset should be used.
         if dir.endswith(".zip"):
             with zipfile.ZipFile(os.path.join(loc, dir), "r") as zip_file:
                 zip_file.extractall("temp")
+                
+                
+    ## All .csv to memory
+    for dir in listdir(loc): # I do this with consideration to multiple csv files in one zip
+        if dir.endswith(".csv"):
+            data[dir.removesuffix(".csv")] = pd.read_csv(os.path.join(loc, dir))
+            
+    ##send to postgres
+    
+    ##close connection, and delete temp
                 
     print("Debug!")     
     
